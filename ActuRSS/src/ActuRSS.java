853b3827cf6f56ff744966e4afff5255ad07038b
bastien.meunier1@gmail.com <bastien.meunier1@gmail.com@e70cf935-b15b-a6f8-7cd1-b338ef7a2bb7>
2013-10-29 19:41:49 +0000

    commits

--- ActuRSS/src/ActuRSS.java
+++ ActuRSS/src/ActuRSS.java
@@ -9,7 +9,7 @@ public class ActuRSS {
 		System.out.println("Hello Moustache");
 		for(int i =0; i<10000; i++)
 		{
-		System.out.println("va te faire enculer bastien");
+		System.out.println("va te faire enculer yann");
 		}
 	}
 
