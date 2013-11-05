32a153b2f612511855904509f0755c4748943a4a
gauthier.cibert@gmail.com <gauthier.cibert@gmail.com@e70cf935-b15b-a6f8-7cd1-b338ef7a2bb7>
2013-11-05 21:43:26 +0000

    Création des classe communes (vraiment pas définitif)

---
+++ ActuRSS/view/src/ca/usherbrooke/ift232/actuRSS/common/Category.java
@@ -0,0 +1,17 @@
+package ca.usherbrooke.ift232.actuRSS.common;
+
+public class Category {
+
+	public String categoryName;
+	
+	public Category(String string) {
+		categoryName = string;
+	}
+	
+	public String toString()
+	{
+		return categoryName;
+		
+	}
+
+}
---
+++ ActuRSS/view/src/ca/usherbrooke/ift232/actuRSS/common/Source.java
@@ -0,0 +1,17 @@
+package ca.usherbrooke.ift232.actuRSS.common;
+
+public class Source {
+
+	private String sourceName;
+	
+	public Source(String string) {
+		// TODO Auto-generated constructor stub
+		sourceName = string;
+	}
+	
+	public String toString()
+	{
+		return sourceName;
+	}
+
+}
