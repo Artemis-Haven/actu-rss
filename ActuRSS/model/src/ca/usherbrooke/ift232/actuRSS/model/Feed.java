a12c3a9425382d87904bb276f6e2717d1205b864
benj.ferre@gmail.com <benj.ferre@gmail.com@e70cf935-b15b-a6f8-7cd1-b338ef7a2bb7>
2013-11-12 20:47:17 +0000

    Revue de code classes News, Feed, Category

--- ActuRSS/model/src/ca/usherbrooke/ift232/actuRSS/model/Feed.java
+++ ActuRSS/model/src/ca/usherbrooke/ift232/actuRSS/model/Feed.java
@@ -4,74 +4,75 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Actu-RSS
- * Couche : Model
- * Date de creation : 2013-10-30
- * Description : 
- *   Classe Feed
- *   Un feed est determine par un nom, une url, une liste de news, une categorie 
- *   
+ * Actu-RSS Couche : Model Date de creation : 2013-10-30 Description : Classe
+ * Feed Un feed est determine par un nom, une url, une liste de news, une
+ * categorie
+ * 
  * @author Benjamin Ferre
  * @author David Boas
  * @version 0.5
  */
 public class Feed {
-	
-	//variables de classe
+
+	// variables de classe
 	private int id;
-	private String name;
+	private String title;
 	private String url;
 	private List<News> listNews = new ArrayList<News>();
-	private Category category;
 
-	//constructeurs
-	public Feed(){
-		this.name = "";
+	// constructeurs
+	public Feed() {
+		this.setTitle("");
 		this.url = "";
 		listNews = null;
 	}
-	
+
 	/**
-	 * @param name le nom du flux
-	 * @param url l'url du flux
-	 * @param entries la liste des news du flux
+	 * @param name
+	 *            le nom du flux
+	 * @param url
+	 *            l'url du flux
+	 * @param entries
+	 *            la liste des news du flux
 	 */
-	public Feed(String name, String url, List<News> entries){
-		this.name = name;
+	public Feed(String name, String url, List<News> entries) {
+		this.setTitle(name);
 		this.url = url;
 		listNews = entries;
 	}
-	//accesseurs
-	public String getNom() {
-		return name;
+
+	// accesseurs
+
+	public String getTitle() {
+		return title;
 	}
+
 	public String getUrl() {
 		return url;
 	}
+
 	public List<News> getListNews() {
 		return listNews;
 	}
-	public Category getCategory() {
-		return category;
-	}
+
 	public int getId() {
 		return id;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
 	public void setUrl(String url) {
 		this.url = url;
-	}	
+	}
+
 	public void setListNews(List<News> entrees) {
 		this.listNews = entrees;
-	}	
-	public void setCategory(Category category) {
-		this.category = category;
 	}
+
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 }
