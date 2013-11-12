750fe5f1a2c644c1dd3517dd7a7067b43f63a225
yann.seree@gmail.com <yann.seree@gmail.com@e70cf935-b15b-a6f8-7cd1-b338ef7a2bb7>
2013-11-12 23:59:02 +0000

    - Ajout des classes de la vue dans final

---
+++ ActuRSS/final/src/ca/usherbrooke/ift232/actuRSS/view/TreeAndList.java
@@ -0,0 +1,79 @@
+package ca.usherbrooke.ift232.actuRSS.view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+
+import ca.usherbrooke.ift232.actuRSS.common.Category;
+import ca.usherbrooke.ift232.actuRSS.common.Source;
+import ca.usherbrooke.ift232.actuRSS.view.treepicker.SourceSelectedEvent;
+import ca.usherbrooke.ift232.actuRSS.view.treepicker.SourceSelectedListener;
+import ca.usherbrooke.ift232.actuRSS.view.treepicker.TreePicker;
+
+public class TreeAndList extends JPanel{
+	
+	JSplitPane splitPane;
+	DefaultListModel<String> listModel;
+	TreePicker tree;
+	
+	public TreeAndList(){
+		
+		
+		this.setLayout(new BorderLayout(1,2));
+		splitPane = new JSplitPane();
+		
+		HashMap<Category, List<Source>> feedByCat = new HashMap<Category, List<Source>>();
+		
+		List<Source> sportSources = new ArrayList<Source>();
+		
+		sportSources.add(new Source("SportSource1"));
+		sportSources.add(new Source("SportSource2"));
+		sportSources.add(new Source("SportSource3"));
+		sportSources.add(new Source("SportSource4"));
+		sportSources.add(new Source("SportSource5"));
+		
+		feedByCat.put(new Category("Sport"), sportSources);
+		List<Source> financeSources = new ArrayList<Source>();
+		financeSources.add(new Source("FinanceSource1"));
+		financeSources.add(new Source("FinanceSource2"));
+		feedByCat.put(new Category("Finance"), financeSources);
+		
+		//================== Ajout du composant à la fenêtre
+		
+		tree = new TreePicker( feedByCat, false);
+		listModel = new DefaultListModel<String>();
+		tree.addSourceSelectedListener(new SourceSelectedListener(){
+
+			@Override
+			public void onSourceSelected(SourceSelectedEvent event) {
+				listModel.removeAllElements();
+				for(int i = 0; i < tree.getSelectedSources().size();i++){
+					listModel.addElement("" + tree.getSelectedSources().get(i) + "\n testttt");
+					System.out.println("Vous avez sélectionné la source: " + tree.getSelectedSources().get(i));
+				}
+				
+			}
+			
+		});
+		
+		JList lst = new JList(listModel);
+		add(splitPane);
+		JScrollPane treescrollPane = new JScrollPane(tree);
+		treescrollPane.setMinimumSize(new Dimension(30,50));
+		treescrollPane.setPreferredSize(new Dimension(75,50));
+		JScrollPane listcrollPane = new JScrollPane(lst);
+		listcrollPane.setMinimumSize(new Dimension(75,50));
+		listcrollPane.setPreferredSize(new Dimension(120,50));
+		splitPane.setLeftComponent(treescrollPane);
+		splitPane.setRightComponent(listcrollPane);
+	}
+
+}
