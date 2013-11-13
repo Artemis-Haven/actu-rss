2828fded85066c3e6558c8b365ea374e08374f7d
artemis.hacktivation@gmail.com <artemis.hacktivation@gmail.com@e70cf935-b15b-a6f8-7cd1-b338ef7a2bb7>
2013-11-13 02:49:40 +0000

    Ajout des fichiers et des classes de la vue dans Trunk. Création d'un faux FeedManager dans Model.

---
+++ trunk/src/ca/usherbrooke/ift232/actuRSS/view/actulist/ActuList.java
@@ -0,0 +1,76 @@
+package ca.usherbrooke.ift232.actuRSS.view.actulist;
+
+import java.util.List;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.EventListenerList;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import ca.usherbrooke.ift232.actuRSS.model.News;
+
+public class ActuList extends JList implements ListSelectionListener
+{
+	public ActuList(List<News> news) {
+
+		super(buildListModelNews(news));
+		
+		
+		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.setSelectedIndex(0);
+		this.addListSelectionListener( this);
+		this.setVisibleRowCount(5);
+		
+		
+	}
+
+	private static ListModel<News> buildListModelNews(List<News> news) {
+		final DefaultListModel<News> listModelNews = new DefaultListModel<News>();
+		fillNews(listModelNews, news);
+
+		return listModelNews;
+	}
+
+	private static void fillNews(DefaultListModel<News> listModel, List<News> news) {
+
+		System.out.println(news);
+		for (News element : news) 
+		{
+			listModel.addElement(element);
+		}
+
+	}
+	
+	// #region ActuSelectedEvent
+
+		private final EventListenerList listenerList = new EventListenerList();
+
+		public void addActuSelectedListener(
+				ActuSelectedListener actuSelectedListener) {
+			listenerList.add(ActuSelectedListener.class, actuSelectedListener);
+		}
+
+		public void removeActuSelectedListener(ActuSelectedListener l) {
+			listenerList.remove(ActuSelectedListener.class, l);
+		}
+
+		protected void fireActuSelectedEvent(ActuSelectedEvent event) {
+			for (ActuSelectedListener l : listenerList
+					.getListeners(ActuSelectedListener.class)) {
+				l.onActuSelected(event);
+			}
+		}
+
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			fireActuSelectedEvent(new ActuSelectedEvent(this, (News) this.getSelectedValue()));
+			
+		}
+
+		// #endregion
+		
+
+}
