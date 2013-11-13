507750e17cab06b5c20c2c2c8ea079a154c96e3d
artemis.hacktivation@gmail.com <artemis.hacktivation@gmail.com@e70cf935-b15b-a6f8-7cd1-b338ef7a2bb7>
2013-11-13 01:21:11 +0000

    Ajout des classes de la BDD dans trunk

--- trunk/src/ca/usherbrooke/ift232/actuRSS/model/Feed.java
+++ trunk/src/ca/usherbrooke/ift232/actuRSS/model/Feed.java
@@ -4,71 +4,76 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Actu-RSS Couche : Model Date de creation : 2013-10-30 Description : Classe
- * Feed Un feed est determine par un nom, une url, une liste de news, une
- * categorie
- * 
+ * Actu-RSS
+ * Couche : Model
+ * Date de creation : 2013-10-30
+ * Description : 
+ *   Classe Feed
+ *   Un feed est determine par un nom, une url, une liste de news, une categorie 
+ *   
  * @author Benjamin Ferre
  * @author David Boas
- * @version 0.5
+ * @version 0.4
  */
 public class Feed {
-
-	// variables de classe
+	
+	//variables de classe
 	private int id;
 	private String title;
 	private String url;
 	private List<News> listNews = new ArrayList<News>();
 
-	// constructeurs
-	public Feed() {
-		this.setTitle("");
+	//constructeurs
+	public Feed(){
+		this.title = "";
 		this.url = "";
 		listNews = null;
 	}
-
+	
 	/**
-	 * @param name
-	 *            le nom du flux
-	 * @param url
-	 *            l'url du flux
-	 * @param entries
-	 *            la liste des news du flux
+	 * @param title le nom du flux
+	 * @param url l'url du flux
+	 * @param entries la liste des news du flux
 	 */
-	public Feed(String name, String url, List<News> entries) {
-		this.setTitle(name);
+	public Feed(int id, String title, String url, List<News> entries){
+		this.id = id;
+		this.title = title;
 		this.url = url;
 		listNews = entries;
 	}
-
-	// accesseurs
-
-	public String getTitle() {
-		return title;
+	
+	public Feed(int id, String title, String url){
+		this.id = id;
+		this.title = title;
+		this.url = url;
+		listNews = null;
 	}
+	//accesseurs
 
 	public String getUrl() {
 		return url;
 	}
-
 	public List<News> getListNews() {
 		return listNews;
 	}
-
 	public int getId() {
 		return id;
 	}
 
+
 	public void setUrl(String url) {
 		this.url = url;
-	}
-
+	}	
 	public void setListNews(List<News> entrees) {
 		this.listNews = entrees;
-	}
+	}	
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
 	}
 
 	public void setTitle(String title) {
