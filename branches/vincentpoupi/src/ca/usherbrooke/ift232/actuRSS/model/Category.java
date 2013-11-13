eada1ad524db109c026b6e06e6eecfb0547109f5
artemis.hacktivation@gmail.com <artemis.hacktivation@gmail.com@e70cf935-b15b-a6f8-7cd1-b338ef7a2bb7>
2013-11-13 03:16:45 +0000

    Création de la branche pour Vincent et Matthieu

---
+++ branches/vincentpoupi/src/ca/usherbrooke/ift232/actuRSS/model/Category.java
@@ -0,0 +1,75 @@
+package ca.usherbrooke.ift232.actuRSS.model;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Actu-RSS
+ * Couche : Model
+ * Date de creation : 2013-10-30
+ * Description : 
+ *   Classe Category
+ *   Une categorie est determinee par un nom, et contient une liste de feeds 
+ *   
+ * @author Benjamin Ferre
+ * @author David Boas
+ * @version 0.4
+ */
+public class Category {
+	
+	//variables de classe
+	private int id;
+	private String name;
+	private List<Feed> listFeed = new ArrayList<Feed>();
+	
+	//constructeurs
+	public Category(){
+		this.id = 0;
+		this.name = "";
+		this.listFeed = null;
+	}
+	
+	/**
+	 * @param name le nom de la categorie
+	 * @param url l'url de la categorie
+	 * @param listFeed la liste des flux de la categorie
+	 */
+	public Category(int id, String name, List<Feed> listFeed){
+		this.id = id;
+		this.name = name;
+		this.listFeed = listFeed;
+	}
+	
+	public Category(int id, String name){
+		this.id = id;
+		this.name = name;
+		this.listFeed =  null;
+	}
+
+	//accesseurs
+	public String getName() {
+		return name;
+	}
+	public List<Feed> getListFeed() {
+		return listFeed;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setListFeed(List<Feed> entries) {
+		listFeed = entries;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
+	}
+}
---
+++ branches/vincentpoupi/src/ca/usherbrooke/ift232/actuRSS/view/Toolbar.java
@@ -0,0 +1,115 @@
+package ca.usherbrooke.ift232.actuRSS.view;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.ComponentOrientation;
+import java.awt.Cursor;
+import java.awt.Dimension;
+
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+
+/**
+ * Actu-RSS
+ * Couche : View
+ * Date de creation : 2013-11-01
+ * Description : 
+ *   La Toolbar est la barre avec les boutons qui
+ *   figure en haut de notre fenetre principale.
+ *   
+ * @author Prenom Nom
+ * @version 0.1
+ */
+ 
+public class Toolbar extends JPanel {
+
+	/**
+	 * Variables privee
+	 * @param Description d'une variable
+	 *
+	 */
+	private JToggleButton allNewsBtn;
+	private JToggleButton unreadNewsBtn;
+	private JToggleButton favNewsBtn;
+	private JButton syncBtn;
+	private JToggleButton readBtn;
+	private JToggleButton favBtn;
+	private ButtonGroup btnGroup;
+	private Menu menuBtn;
+	private JPanel leftPanel;
+	private JPanel rightPanel;
+
+	/**
+	 * Constructeur
+	 * 
+	 */
+	public Toolbar(){
+
+		this.setLayout(new BorderLayout());
+		
+		leftPanel = new JPanel();
+		
+		syncBtn = new JButton(new ImageIcon(getClass().getResource("/img/sync.png")));
+		syncBtn.setBorderPainted(false);
+		syncBtn.setFocusPainted(false);
+		leftPanel.add(syncBtn);
+
+		buildButtonGroup();
+		
+		rightPanel = new JPanel();
+		rightPanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		rightPanel.setOpaque(false);
+		rightPanel.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		rightPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		rightPanel.setSize(new Dimension(24, 24));
+		
+		readBtn = buildToggleButton("/img/read.png");
+		rightPanel.add(readBtn);
+		
+		favBtn = buildToggleButton("/img/favorite.png");
+		rightPanel.add(favBtn);
+		
+		menuBtn = new Menu();
+		menuBtn.setMaximumSize(new Dimension(24, 24));
+		menuBtn.setRequestFocusEnabled(false);
+		menuBtn.setOpaque(false);
+		menuBtn.setIgnoreRepaint(true);
+		menuBtn.setBorderPainted(false);
+		menuBtn.setAlignmentY(Component.CENTER_ALIGNMENT);
+		menuBtn.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		rightPanel.add(menuBtn);
+
+		this.add(leftPanel, BorderLayout.WEST);
+		this.add(rightPanel, BorderLayout.EAST);
+	}
+
+	private void buildButtonGroup() {
+		allNewsBtn = new JToggleButton("Tout", true);
+		unreadNewsBtn = new JToggleButton("Non lus");
+		favNewsBtn = new JToggleButton("Favoris");
+		
+		btnGroup = new ButtonGroup();
+		btnGroup.add(allNewsBtn);
+		btnGroup.add(unreadNewsBtn);
+		btnGroup.add(favNewsBtn);
+
+		allNewsBtn.setFocusPainted(false);
+		unreadNewsBtn.setFocusPainted(false);
+		favNewsBtn.setFocusPainted(false);
+		
+		leftPanel.add(allNewsBtn, BorderLayout.WEST);
+		leftPanel.add(unreadNewsBtn, BorderLayout.WEST);
+		leftPanel.add(favNewsBtn, BorderLayout.WEST);
+	}
+
+	private JToggleButton buildToggleButton(String path) {
+		JToggleButton btn = new JToggleButton(new ImageIcon(getClass().getResource(path)));
+		btn.setBorderPainted(false);
+		btn.setFocusPainted(false);
+		return btn;
+	}
+}
