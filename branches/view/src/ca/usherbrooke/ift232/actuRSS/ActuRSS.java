3bc8e3e21cf90f8da2ad00f5ddc5f0e098095a08
yann.seree@gmail.com <yann.seree@gmail.com@e70cf935-b15b-a6f8-7cd1-b338ef7a2bb7>
2013-11-19 23:20:29 +0000

    Supression du model dans la vue et creation du getter du pannel

--- branches/view/src/ca/usherbrooke/ift232/actuRSS/ActuRSS.java
+++ branches/view/src/ca/usherbrooke/ift232/actuRSS/ActuRSS.java
@@ -1,5 +1,6 @@
 package ca.usherbrooke.ift232.actuRSS;
 
+import ca.usherbrooke.ift232.actuRSS.controller.Controller;
 import ca.usherbrooke.ift232.actuRSS.model.Model;
 import ca.usherbrooke.ift232.actuRSS.view.View;
 
@@ -12,8 +13,8 @@ public class ActuRSS {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Moustache");
 		Model model = new Model();
-		View view  = new View(model);
-		//Controller controller = new Controller(model,view);
+		View view  = new View();
+		Controller controller = new Controller(model,view);
 		
 		view.setVisible(true);
 	}
