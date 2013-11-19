1e254ce658d4ba4cc72b27f2e103c54eab95e43c
artemis.hacktivation <artemis.hacktivation@gmail.com>
2013-11-19 23:14:17 +0000

    Création de la classe RssParser

---
+++ src/ca/usherbrooke/ift232/actuRSS/model/RssParser.java
@@ -0,0 +1,14 @@
+package ca.usherbrooke.ift232.actuRSS.model;
+
+import java.io.File;
+
+import ca.usherbrooke.ift232.actuRSS.Feed;
+
+public class RssParser {
+
+	public Feed parse(File f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
