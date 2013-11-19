a1c300d8a2efc17117a757f62989f307111f8c6a
artemis.hacktivation <artemis.hacktivation@gmail.com>
2013-11-19 22:15:00 +0000

    Création de la classe Model et màj du FeedManager

--- src/ca/usherbrooke/ift232/actuRSS/model/Model.java
+++ src/ca/usherbrooke/ift232/actuRSS/model/Model.java
@@ -1,9 +1,13 @@
 package ca.usherbrooke.ift232.actuRSS.model;
 
+import java.io.File;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+
+import javax.swing.text.html.parser.Parser;
 
 import ca.usherbrooke.ift232.actuRSS.Category;
 import ca.usherbrooke.ift232.actuRSS.Feed;
@@ -15,11 +19,14 @@ public class Model {
 
 	DatabaseManager dbManager;
 	FeedManager feedManager;
+	RssParser parser;
 	
 	public Model() {
 
 		dbManager = new DatabaseManager(new Database("acturss.db"));
-		dbManager.createDB();
+		feedManager = new FeedManager();
+		
+		/*dbManager.createDB();
 
 		News n1 = new News("titre1", "url1", "author1", new GregorianCalendar(), "contents1", false, false);
 		News n2 = new News("titre2", "url2", "author2", new GregorianCalendar(), "contents2", false, false);
@@ -55,8 +62,63 @@ public class Model {
 
 		feedManager = new FeedManager(catList, catList);
 		
-		dbManager.insertObjetToDB(feedManager.getListCategory());
+		dbManager.insertObjetToDB(feedManager.getListCategory());*/
+		
+	}
+	
+	private void loadAllFromDB() {
+		try {
+			feedManager.setOldListCategory(dbManager.getAllCategories());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Impossible d'accéder à la BD.");
+			e.printStackTrace();
+		}
+	}
+	
+	private void sendAllToDB() {
+		//vider la BdD avec clearDB
+		//dbManager.clearDB();
+		dbManager.insertObjetToDB(feedManager.getOldListCategory());
+	}
+	
+	public void synchronize() {
+		
+		//Si la liste des catégories est vide, on la charge depuis la BD
+		if(feedManager.getOldListCategory().isEmpty()) {
+			loadAllFromDB();
+		}
+		ArrayList<Category> newList = new ArrayList<Category>();
+		
+		// Pour chaque feed dans chaque catégorie :
+		for(Category category : feedManager.getOldListCategory()) {
+			Category newCat = new Category();
+			
+			for(Feed feed : category.getListFeed()) {
+				try {
+					// On récupère les fichiers via leur url, 
+					// on les parse 
+					// et on les envoie dans le feedManager
+					File f = new File(feed.getUrl());
+					Feed newFeed = parser.parse(f);
+					newCat.getListFeed().add(newFeed);
+				} catch (Exception e) {
+					// TODO: handle exception
+					System.out.println("Problème de connexion à internet.");
+				}
+			}
+			newList.add(newCat);
+		}
+		feedManager.setListCategory(newList);
+		//Ici, ListCategory contient la nouvelle liste de flux
+		//et oldListCategory contient l'ancienne
+		
+		//On fusionne les deux listes
+		feedManager.merge();
+		
+		//On
+		sendAllToDB();
+		
 		
 	}
 }
-
---
+++ src/ca/usherbrooke/ift232/actuRSS/model/RssParser.java
@@ -0,0 +1,16 @@
+package ca.usherbrooke.ift232.actuRSS.model;
+
+import java.io.File;
+
+import ca.usherbrooke.ift232.actuRSS.Feed;
+import ca.usherbrooke.ift232.actuRSS.News;
+
+public class RssParser {
+
+	public Feed parse(File f) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+}
