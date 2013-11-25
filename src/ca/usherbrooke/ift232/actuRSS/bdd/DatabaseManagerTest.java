6b3ea05102b3fbb3d026c1441d8d3cdba25b7495
artemis.hacktivation <artemis.hacktivation@gmail.com>
2013-11-25 20:33:49 +0000

    Ajout de tests d'acceptation (ne passe pas !)

--- src/ca/usherbrooke/ift232/actuRSS/bdd/DatabaseManagerTest.java
+++ src/ca/usherbrooke/ift232/actuRSS/bdd/DatabaseManagerTest.java
@@ -60,7 +60,7 @@ public class DatabaseManagerTest {
 		
 		Feed feed1 = new Feed(1, "Le Monde", "www.lemonde.fr/rss/une/xml");
 		Feed feed2 = new Feed(2, "Eurosport", "http://www.eurosport.fr/rss.xml");
-		Feed feed3 = new Feed(3, "L'équipe", "http://www.lequipe.fr/rss/actu_rss.xml");
+		Feed feed3 = new Feed(3, "L'ï¿½quipe", "http://www.lequipe.fr/rss/actu_rss.xml");
 		ArrayList<Feed> listFeedExpected = new ArrayList<Feed>();
 		
 		Category category1 = new Category(1, "France");
@@ -93,7 +93,6 @@ public class DatabaseManagerTest {
 		dbManager.insertObjetToDB(listCategoryExpected);
 		
 		
-		DatabaseUtil dbUtil = dbManager.getDbUtil();
 		try {
 			ArrayList<Category> listCategory = dbManager.getAllCategories();
 			for(int i=0;i<listCategory.size();i++)
@@ -112,7 +111,7 @@ public class DatabaseManagerTest {
 						assertEquals(listNewsExpected.get(k).getUrl(), listCategory.get(i).getListFeed().get(j).getListNews().get(k).getUrl());
 						assertEquals(listNewsExpected.get(k).getTitle(), listCategory.get(i).getListFeed().get(j).getListNews().get(k).getTitle());
 						assertEquals(listNewsExpected.get(k).getAuthor(), listCategory.get(i).getListFeed().get(j).getListNews().get(k).getAuthor());
-						assertEquals(dbUtil.ConvertCalendarToString(listNewsExpected.get(k).getDate()), dbUtil.ConvertCalendarToString(listCategory.get(i).getListFeed().get(j).getListNews().get(k).getDate()));
+						assertEquals(DatabaseUtil.ConvertCalendarToString(listNewsExpected.get(k).getDate()), DatabaseUtil.ConvertCalendarToString(listCategory.get(i).getListFeed().get(j).getListNews().get(k).getDate()));
 						assertEquals(listNewsExpected.get(k).getContents(), listCategory.get(i).getListFeed().get(j).getListNews().get(k).getContents());
 					
 						
@@ -132,7 +131,10 @@ public class DatabaseManagerTest {
 	@Test
 	public void testInsertCategory() {
 				
-		Category category1 = new Category(1, "France");
+		ArrayList<Feed> list = new ArrayList<Feed>();
+		Feed feed = new Feed(0, "titre de mon feed", "url de mon feed");
+		list.add(feed);
+		Category category1 = new Category(1, "France", list);
 		Category category2 = new Category(2, "Sport");
 		Category category3 = new Category(3, "Culture");
 			
@@ -151,9 +153,9 @@ public class DatabaseManagerTest {
 			{
 				assertEquals(listCategoryExpected.get(i).getId(), listCategory.get(i).getId());
 				assertEquals(listCategoryExpected.get(i).getName(), listCategory.get(i).getName());
-				
 			
 			}
+			assertEquals(listCategoryExpected.get(0).getListFeed().get(0).getTitle(), feed.getTitle() );
 			
 		} 
 		catch (SQLException e) {
@@ -168,7 +170,7 @@ public class DatabaseManagerTest {
 		Category category1 = new Category(1, "France");
 		dbManager.insertCategory(category1);
 		
-		Feed feed1 = new Feed(1, "Le Monde", "www.lemonde.fr/rss/une/xml");
+		Feed feed1 = new Feed(1, "Le Monde", "www.le\\\"monde.fr/rss/une/xml");
 		Feed feed2 = new Feed(2, "Eurosport", "http://www.eurosport.fr/rss.xml");
 		Feed feed3 = new Feed(3, "L'ï¿½quipe", "http://www.lequipe.fr/rss/actu_rss.xml");
 		
@@ -201,9 +203,7 @@ public class DatabaseManagerTest {
 
 	@Test
 	public void testInsertNews() {
-		
-		DatabaseUtil dbUtil = dbManager.getDbUtil();
-		
+				
 		Category category1 = new Category(1, "France");
 		dbManager.insertCategory(category1);
 		
@@ -212,14 +212,14 @@ public class DatabaseManagerTest {
 		Calendar date1 = Calendar.getInstance();
 		date1.set(2013, 11, 12, 05, 05, 05);
 				
-		News news1 = new News("Football - Ligue 1 - L1 : Le top 10 des idï¿½es reï¿½uesï¿½ battues en brï¿½che", "http://www.eurosport.fr/football/ligue-1/2013-2014/l1-le-top-10-des-idees-recues.-battues-en-breche_sto4004002/story.shtml", "Ta mï¿½re", date1 ,"Championnat homogï¿½ne, gardiens infranchissables, Paris et Monaco seuls au monde : la Ligue 1 charrie des clichï¿½s qui ne correspondent pas ï¿½ sa rï¿½alitï¿½. Notre top 10.", true, true  );
-		//News news2 = new News("Football - Ligue 1 - L1 : Le top 10 des idï¿½es reï¿½uesï¿½ battues en brï¿½che", "http://www.eurosport.fr/football/ligue-1/2013-2014/l1-le-top-10-des-idees-recues.-battues-en-breche_sto4004003/story.shtml", "Ta mï¿½re", date1 ,"Championnat homogï¿½ne, gardiens infranchissables, Paris et Monaco seuls au monde : la Ligue 1 charrie des clichï¿½s qui ne correspondent pas ï¿½ sa rï¿½alitï¿½. Notre top 10.", false, false  );
+		News news1 = new News("Football - Ligue 1 - L1 : Le top 10 des idï¿½es reï¿½uesï¿½ battues en brï¿½che", "http://www.eurosport.fr\\/foo\"tb'all/li<gue-1/20>13-2014/l1-le-top-10-des-idees-recues.-battues-en-breche_sto4004002/story.shtml", "Ta mï¿½re", date1 ,"Championnat homogï¿½ne, gardiens infranchissables, Paris et Monaco seuls au monde : la Ligue 1 charrie des clichï¿½s qui ne correspondent pas ï¿½ sa rï¿½alitï¿½. Notre top 10.", true, true  );
+		News news2 = new News("Football - Ligue 1 - L1 : Le top 10 des idï¿½es reï¿½uesï¿½ battues en brï¿½che", "http://www.eurosport.fr/football/ligue-1/2013-2014/l1-le-top-10-des-idees-recues.-battues-en-breche_sto4004003/story.shtml", "Ta mï¿½re", date1 ,"Championnat homogï¿½ne, gardiens infranchissables, Paris et Monaco seuls au monde : la Ligue 1 charrie des clichï¿½s qui ne correspondent pas ï¿½ sa rï¿½alitï¿½. Notre top 10.", false, false  );
 		
 		ArrayList<News> listNewsExpected = new ArrayList<News>();
 		dbManager.insertNews(news1, 1);
-		//dbManager.insertNews(news2, 1);
+		dbManager.insertNews(news2, 1);
 		listNewsExpected.add(news1);
-		//listNewsExpected.add(news2);
+		listNewsExpected.add(news2);
 		try {
 			ArrayList<Category> listCategory = dbManager.getAllCategories();
 			for(int i=0;i<listCategory.get(0).getListFeed().get(0).getListNews().size();i++)
@@ -227,7 +227,7 @@ public class DatabaseManagerTest {
 				assertEquals(listNewsExpected.get(i).getUrl(), listCategory.get(0).getListFeed().get(0).getListNews().get(i).getUrl());
 				assertEquals(listNewsExpected.get(i).getTitle(), listCategory.get(0).getListFeed().get(0).getListNews().get(i).getTitle());
 				assertEquals(listNewsExpected.get(i).getAuthor(), listCategory.get(0).getListFeed().get(0).getListNews().get(i).getAuthor());
-				assertEquals(dbUtil.ConvertCalendarToString(listNewsExpected.get(i).getDate()), dbUtil.ConvertCalendarToString(listCategory.get(0).getListFeed().get(0).getListNews().get(i).getDate()));
+				assertEquals(DatabaseUtil.ConvertCalendarToString(listNewsExpected.get(i).getDate()), DatabaseUtil.ConvertCalendarToString(listCategory.get(0).getListFeed().get(0).getListNews().get(i).getDate()));
 				assertEquals(listNewsExpected.get(i).getContents(), listCategory.get(0).getListFeed().get(0).getListNews().get(i).getContents());
 			}
 			
