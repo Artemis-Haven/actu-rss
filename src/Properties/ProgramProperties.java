c20d28356b3919baed52571f9730a5070b511565
bastien.meunier1 <bastien.meunier1@gmail.com>
2013-11-26 21:10:14 +0000

    fichier Propriétés

---
+++ src/Properties/ProgramProperties.java
@@ -0,0 +1,77 @@
+package Properties;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ProgramProperties extends Properties {
+
+	final static public String DEFAULT_FILE_NAME = "default.properties";
+	final static public String USER_FILE_NAME = "user.properties";
+
+	static private ProgramProperties instance__;
+
+	static synchronized public ProgramProperties getInstance() {
+
+		if (instance__ == null) {
+
+			// create and load default properties
+			Properties defaultProps = new Properties();
+			defaultProps.setProperty("NewsNumber", "20");
+			defaultProps.setProperty("Afficher", "Tous");
+			FileInputStream in;
+
+			try { 
+
+				in = new FileInputStream(DEFAULT_FILE_NAME);
+				defaultProps.load(in);
+				in.close();
+
+			} catch (FileNotFoundException e2) {
+			} catch (IOException e3) {
+			}
+			// create program properties with default
+			ProgramProperties applicationProps; 
+
+			applicationProps = new ProgramProperties(defaultProps);
+
+			try { 
+
+				// now load properties from last invocation
+				in = new FileInputStream(USER_FILE_NAME);
+				applicationProps.load(in);
+				in.close(); 
+
+			} catch (FileNotFoundException e) {
+			} catch (IOException e1) {
+			}
+
+			instance__ = applicationProps;
+
+		} // end if
+
+		return instance__;
+	}
+
+	private ProgramProperties(Properties defaultProps){
+		super(defaultProps);
+	}
+	
+	public void save(){
+
+		FileOutputStream out;
+
+		try {
+
+			out = new FileOutputStream(USER_FILE_NAME);
+			this.store(out, "---No Comment---");
+			out.close();
+
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+	}
+}
+
