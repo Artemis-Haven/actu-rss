07a0156c0c827835e99eccf5951e1883692c1c71
bastien.meunier1 <bastien.meunier1@gmail.com>
2013-11-26 21:38:20 +0000

    modification des toString et du ScrollPanel

--- src/ca/usherbrooke/ift232/actuRSS/Category.java
+++ src/ca/usherbrooke/ift232/actuRSS/Category.java
@@ -70,6 +70,6 @@ public class Category {
 
 	@Override
 	public String toString() {
-		return "Category [id=" + id + ", name=" + name + "]";
+		return "Category" ;//[id=" + id + ", name=" + name + "]";
 	}
 }
