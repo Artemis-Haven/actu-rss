a61bada79ff9342e1d315c59ae71c8c3aec34263
artemis.hacktivation <artemis.hacktivation@gmail.com>
2013-12-03 00:20:26 +0000

    Vider le trunk

--- src/ca/usherbrooke/ift232/actuRSS/ActuRssUtils.java
+++
@@ -1,5 +0,0 @@
-package ca.usherbrooke.ift232.actuRSS;
-
-public class ActuRssUtils {
-
-}
--- src/ca/usherbrooke/ift232/actuRSS/view/actulist/ActuSelectedListener.java
+++
@@ -1,9 +0,0 @@
-package ca.usherbrooke.ift232.actuRSS.view.actulist;
-
-
-import java.util.EventListener;
-
-public interface ActuSelectedListener extends EventListener
-{
-	public void onActuSelected(ActuSelectedEvent event);
-}
