b07bf6fdb31fbe24666c93f98bf2a6cbcd22d3cc
javakingston2 <javakingston2@gmail.com>
2013-12-04 01:07:41 +0000

    Ajouts de comments pour la javadoc

--- src/ca/usherbrooke/ift232/actuRSS/properties/ParamDialog.java
+++ src/ca/usherbrooke/ift232/actuRSS/properties/ParamDialog.java
@@ -5,6 +5,12 @@ import javax.swing.JFrame;
 
 public abstract class ParamDialog extends JDialog {
 
+	/**
+	 * Permet d'initialiser les paramètres du Dialog
+	 * @param parent
+	 * @param title
+	 * @param modal
+	 */
 	public ParamDialog(JFrame parent, String title, boolean modal){
 		super(parent, title, modal);
 		this.setSize(400, 300);
@@ -13,10 +19,16 @@ public abstract class ParamDialog extends JDialog {
 		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 	}
 
+	/**
+	 * Permet de montrer le Dialog
+	 */
 	public void showDialog(){
 		this.setVisible(true);
 	}
 	
+	/**
+	 * Permet de fermer le Dialog
+	 */
 	public void closeDialog(){
 		this.dispose();
 	}
