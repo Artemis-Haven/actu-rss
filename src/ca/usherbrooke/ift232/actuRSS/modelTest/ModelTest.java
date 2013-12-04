96278c6935133262efdc28ac24aeca256d9c4737
boas.dav <boas.dav@gmail.com>
2013-12-04 00:57:18 +0000

    Commit de obtainsDocument

--- src/ca/usherbrooke/ift232/actuRSS/modelTest/ModelTest.java
+++ src/ca/usherbrooke/ift232/actuRSS/modelTest/ModelTest.java
@@ -1,29 +1,35 @@
 package ca.usherbrooke.ift232.actuRSS.modelTest;
 
 import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
 import org.junit.Test;
 
-public class ModelTest {
+import ca.usherbrooke.ift232.actuRSS.model.Model;
+
+import org.w3c.dom.Document;
+
+public class ModelTest extends TestCase {
 
         @Test
-        public void testModel() {
-                fail("Not yet implemented");
+        public void testModel()  {
+            fail("Not yet implemented");
         }
 
         @Test
         public void testSynchronize() {
-                fail("Not yet implemented");
+            fail("Not yet implemented");
         }
 
         @Test
         public void testNotifyObserver() {
-                fail("Not yet implemented");
+            fail("Not yet implemented");
         }
 
         @Test
         public void testObtainDocument() {
-                fail("Not yet implemented");
+            String feedurl = "http://feeds2.feedburner.com/Pressecitron";
+            assertTrue(Model.obtainDocument(feedurl) instanceof Document);
         }
 
 }
