56ec306b8743d6c1a807893fed55d759a84e946e
benj.ferre@gmail.com <benj.ferre@gmail.com@e70cf935-b15b-a6f8-7cd1-b338ef7a2bb7>
2013-12-14 21:06:19 +0000

    Refactorisation des constructeurs des classes Feed, News, Category

--- trunk/src/ca/usherbrooke/ift232/actuRSS/Category.java
+++ trunk/src/ca/usherbrooke/ift232/actuRSS/Category.java
@@ -4,78 +4,81 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Actu-RSS
- * Couche : Model
- * Date de creation : 2013-10-30
- * Description : 
- *   Classe Category
- *   Une categorie est determinee par un nom, et contient une liste de feeds 
- *   
+ * Actu-RSS Couche : Model Date de creation : 2013-10-30 Description : Classe
+ * Category Une categorie est determinee par un nom, et contient une liste de
+ * feeds
+ * 
  * @author Benjamin Ferre
  * @author David Boas
  * @version 0.4
  */
 public class Category {
-	
-	//variables de classe
+
+	// variables de classe
 	private int id;
 	private String name;
 	private List<Feed> listFeed;
-	
-	//constructeurs
+
+	// constructeurs
 	/**
 	 * Category : constructeur
 	 */
-	public Category(){
-		this.id = -1;
-		this.name = "";
-		this.listFeed = new ArrayList<Feed>();
+	public Category() {
+		this(-1, "", new ArrayList<Feed>());
 	}
-	
-	/**Constructeurs
-	 * @param name le nom de la categorie
-	 * @param url l'url de la categorie
-	 * @param listFeed la liste des flux de la categorie
+
+	/**
+	 * Constructeurs
+	 * 
+	 * @param name
+	 *            le nom de la categorie
+	 * @param url
+	 *            l'url de la categorie
+	 * @param listFeed
+	 *            la liste des flux de la categorie
 	 */
-	public Category(int id, String name, List<Feed> listFeed){
+	public Category(int id, String name, List<Feed> listFeed) {
 		this.id = id;
 		this.name = name;
 		this.listFeed = listFeed;
 	}
-	
-	public Category(int id, String name){
-		this.id = id;
-		this.name = name;
-		this.listFeed = new ArrayList<Feed>();
+
+	public Category(int id, String name) {
+		this(id, name, new ArrayList<Feed>());
 	}
 
-	//accesseurs
+	// accesseurs
 	public String getName() {
 		return name;
 	}
+
 	public List<Feed> getListFeed() {
 		return listFeed;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public void setListFeed(List<Feed> entries) {
 		listFeed = entries;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
 
 	@Override
 	public String toString() {
-		/*String resultat = "\n\nCategory [id=" + id + ", name=" + name + "]";
-		for(Feed f : this.getListFeed()) {
-			resultat +=  "\n" + f;
-		}
-		return resultat;*/
+		/*
+		 * String resultat = "\n\nCategory [id=" + id + ", name=" + name + "]";
+		 * for(Feed f : this.getListFeed()) { resultat += "\n" + f; } return
+		 * resultat;
+		 */
 		return this.getName();
 	}
 }
