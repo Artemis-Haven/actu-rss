e1039e9a4378f4b46bf03af3a542b2618cb05b93
yann.seree@gmail.com <yann.seree@gmail.com@e70cf935-b15b-a6f8-7cd1-b338ef7a2bb7>
2013-12-21 22:36:48 +0000

    avancement du desing pattern command

--- trunk/src/ca/usherbrooke/ift232/actuRSS/controller/command/ActionResetPref.java
+++ trunk/src/ca/usherbrooke/ift232/actuRSS/controller/command/ActionResetPref.java
@@ -4,12 +4,23 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 
+import ca.usherbrooke.ift232.actuRSS.view.parameters.ViewChangeProperties;
+
 public class ActionResetPref extends AbstractAction {
 
+	private ViewChangeProperties pref;
+	
+	public ActionResetPref(ViewChangeProperties prefHook) {
+		super();		
+		this.pref = prefHook;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		pref.renewDialog();
 		
 	}
+
+
 
 }
